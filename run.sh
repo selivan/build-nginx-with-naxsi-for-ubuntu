a0eb54a75b59a2b1783080b54f2afdd677289c8c
Pavel Selivanov <selivan.at.github@gmail.com>
2020-11-25 22:48:21 +0300

    WIP

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd /root
 
 set -x
 apt source nginx
-nginx_src=$(find . -type d -name 'nginx-*')
+nginx_src=$(readlink -f $(find . -type d -name 'nginx-*' | head -1))
 
 curl -L -O https://github.com/nbs-system/naxsi/archive/${NAXSI_VERSION}.tar.gz && \
 tar xzf ${NAXSI_VERSION}.tar.gz && \
@@ -45,7 +45,7 @@ Depends: \${misc:Depends}, \${shlibs:Depends}\n\
 Description: WAF Naxsi\n" >> control
 
 cat rules | \
-perl -pe 's/DYN_MODS := \\\n/DYN_MODS := \\\nhttp_naxsi \\\n/s' | \
+perl -pe 's/DYN_MODS := \\\n/DYN_MODS := \\\n\thttp-naxsi \\\n/s' | \
 perl -pe 's/--with-http_dav_module \\\n//s' | \
 perl -pe 's/--without-http_geo_module \\\n//s' | \
 perl -pe 's/--without-http_limit_req_module \\\n//s' | \
@@ -57,14 +57,51 @@ mv rules rules.bak
 mv rules.new rules
 
 . /etc/os-release && \
-echo -n "nginx (${NGINX_BUILD_VERSION}) ${VERSION_CODENAME}; urgency=medium\n\
+echo -ne "nginx (${NGINX_BUILD_VERSION}+naxsi${NAXSI_VERSION}) ${VERSION_CODENAME}; urgency=medium\n\
 \n\
   * Custom build with WAF Naxsi\n\
 \n\
  -- Build with Docker image by Pavel Selivanov <selivan.at.github@gmail.com>  $(date +'%a, %d %b %Y %H:%M:%S %z')\n\n" \
->> changelog.new && \
+> changelog.new && \
 mv changelog changelog.old && \
 cat changelog.new changelog.old > changelog
+
+echo "load_module modules/ngx_http_naxsi_module.so;" > libnginx-mod.conf/mod-http-naxsi.conf
+
+cat <<EOF > modules/watch/http-naxsi
+version=4
+opts="dversionmangle=s/v//,filenamemangle=s%(?:.*?)?v?(\d[\d.]*)\.tar\.gz%libnginx-mod-http-echo-$1.tar.gz%" \
+    https://github.com/nbs-system/naxsi/tags \
+    (?:.*?/)?v?(\d[\d.]*)\.tar\.gz debian debian/ngxmod uupdate http-naxsi
+EOF
+
+#./debian/modules/control:Module: http-echo
+
+cat << EOF > modules/control
+
+Module: naxsi
+Homepage: https://github.com/nbs-system/naxsi
+Files-Excluded: .travis.yml
+Version: ${NAXSI_VERSION}
+
+EOF
+
+cat << EOF > libnginx-mod-http-naxsi.nginx
+#!/usr/bin/perl -w
+
+use File::Basename;
+
+# Guess module name
+$module = basename($0, '.nginx');
+$module =~ s/^libnginx-mod-//;
+
+$modulepath = $module;
+$modulepath =~ s/-/_/g;
+
+print "mod debian/build-extras/objs/ngx_${modulepath}_module.so\n";
+print "mod debian/libnginx-mod.conf/mod-${module}.conf\n";
+
+EOF
 
 cd ..
 
